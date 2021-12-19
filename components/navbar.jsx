03a4eac1d1459c68b2552601a281229fc88a3414
Sahil Aujla <sahilaujla15@gmail.com>
2021-12-19 08:47:06 +0530

    Made the Navbar Component.

---
+++ components/navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="grid grid-cols-1 items-center justify-center w-screen h-1/6 bg-nav-color">
+      <h1 className="text-5xl font-bold text-center px-auto py-5">
+        removethis.nft
+      </h1>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "nav-color": "#2A2E35",
+        "body-color": "#12181B",
+        "upgrade-outline": "#F96A1F",
+        "refer-outline": "#09C372",
+        "badge-bg": "#2A2E35",
+        "gr-start": "#FA8142",
+        "gr-end": "#FF4088",
+      },
+    },
+  },
+  plugins: [],
+};
